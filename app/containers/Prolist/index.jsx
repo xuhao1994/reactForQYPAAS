08d18458c0a83924272450acccb74ff1755003bd
xuhao1994 <1021064377@qq.com>
2017-10-09 18:20:28 +0800

    机构列表页写了一点

--- app/containers/Prolist/index.jsx
+++ app/containers/Prolist/index.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import HeaderDemo1 from '../../components/HeaderDemo1'
+import TCDemo1 from '../../components/TCDemo1'
 import './style.css'
 
 class Prolist extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            data:[]
+        }
     }
     render() {
-    	var data = {
-    		
-    	}
-
-
-
-
         return (
-            <div>
+            <div className="ProListBox">
                 <HeaderDemo1/>
-            	{this.props.params.key}
+                {
+                    this.state.data.length
+                    ?
+                    this.state.data.map((ele,index)=>{
+                        return <TCDemo1 
+                                key={index}
+                                img={ele.img} 
+                                name={ele.name} 
+                                score={ele.score}
+                                hName={ele.hName}
+                                distance={ele.distance}
+                                price={ele.price}
+                                oldPrice={ele.oldPrice}
+                            />
+                    })
+                    :
+                    <div>加载中...</div>
+                }
             </div>
         )
     }
